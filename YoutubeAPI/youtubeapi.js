d836a9f7ff12f8c0b98c797fc36b4acace645731
yogashri1126 <ypradhan.pge@texas.edu>
2017-08-03 20:08:26 -0500

    testing youtubeapi

---
+++ YoutubeAPI/youtubeapi.js
@@ -0,0 +1,30 @@
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+
+function onYouTubeApiLoad() {
+    // PUT YOUR API KEY HERE.
+    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+
+    search();
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        part: 'q'
+    });
+    
+    request.execute(onSearchResponse);
+}
+
+function onSearchResponse(response) {
+    showResponse(response);
+}
